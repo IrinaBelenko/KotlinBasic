4525e76bf905804fc38119e0dc94f72368e75fe6
IrinaBelenko <akaiguren@gmail.com>
2024-04-14 13:43:09 +0300

    interface

---
+++ src/main/kotlin/lesson_15/lesson_15.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1_10.lesson_14
+
+import lesson_15.Upsilon
+
+fun main() {
+    
+    val upsilonShuttle = Upsilon(50)
+    upsilonShuttle.runDiagnostic()
+    upsilonShuttle.prepareForTakeoff()
+    upsilonShuttle.prepareForLanding()
+    upsilonShuttle.startShooting()
+    upsilonShuttle.reloadGuns()
+
+}
+
+
